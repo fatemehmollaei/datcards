98671d1d659448b0e5e3bd5d2f01457660223e9c
mivamax company <fatemeh.mollaei.official@gmail.com>
2023-04-20 17:31:01 +0430

    update

--- config/server.js
+++ config/server.js
@@ -1,23 +1,23 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   url: env('BACKEND_URL_LOCAL'),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port:  7225,
-  url: 'https://app.datcards.ir',
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('BACKEND_URL_LOCAL'),
   app: {
-    keys: ['cURpc6rxVehm8fOKccX2nA==','XP2wz3YeOiLuZ0VYImFKeA==','uy9WbZrWPSqWMmetXPnS0Q==','nLMU5mojuWW6Cyz8AA0/kA==']
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false)
-  }
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
+
+// module.exports = ({ env }) => ({
+//   host: '0.0.0.0',
+//   port:  7225,
+//   url: 'https://app.datcards.ir',
+//   app: {
+//     keys: ['cURpc6rxVehm8fOKccX2nA==','XP2wz3YeOiLuZ0VYImFKeA==','uy9WbZrWPSqWMmetXPnS0Q==','nLMU5mojuWW6Cyz8AA0/kA==']
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false)
+//   }
+// });
